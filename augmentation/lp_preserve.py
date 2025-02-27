d43af3fb4e67bfd37c741e857c90b4cbd41f8a5e
Chendi Qian <1049653012@qq.com>
2025-02-27 17:08:17 +0100

    update neg

--- augmentation/lp_preserve.py
+++ augmentation/lp_preserve.py
@@ -75,31 +75,27 @@ class AddRedundantConstraint:
     def neg(self, data: HeteroData, negatives: int) -> List[HeteroData]:
         m, n = data['cons'].num_nodes, data['vals'].num_nodes
         num_new_cons = int(m * self.p)
-        rand_mat = sp.random_array((num_new_cons * negatives, m), density=self.affinity / m, format='csr')
-        rand_mat.data *= -1.  # random_array generates (0, 1)
         edge_index = data[('cons', 'to', 'vals')].edge_index.numpy()
         A = sp.csr_array((data[('cons', 'to', 'vals')].edge_attr.numpy().squeeze(1),
                           (edge_index[0], edge_index[1])), shape=(m, n))
-        merged_A_new = rand_mat @ A  # (negatives * new adj) new rows
-        merged_extra_b = rand_mat @ data.b.numpy()
 
         neg_samples = []
+        # todo: parallelize the matmul
         for i in range(negatives):
-            row_start = i * num_new_cons
-            row_end = (i + 1) * num_new_cons
-            A_new = merged_A_new[row_start: row_end].tocoo()
+            eye_mat = sp.diags_array(np.random.randn(m), format='csr')
+            rand_mat = sp.random_array((num_new_cons, m), density=self.affinity / m, format='csr')
+            rand_mat.data = np.random.randn(*rand_mat.data.shape)
+            mat = sp.vstack([eye_mat, rand_mat])
 
-            new_edge_index = np.vstack([A_new.row, A_new.col])
-            new_edge_attr = A_new.data
-            new_edge_index[0] += m
+            A_new = (mat @ A).tocoo()
+            edge_index = torch.from_numpy(np.vstack([A_new.row, A_new.col])).long()
+            edge_attr = torch.from_numpy(A_new.data)[:, None].float()
 
-            extra_b = merged_extra_b[row_start: row_end]
-            new_b = torch.cat([data.b, torch.from_numpy(extra_b).float()], dim=0)
-
-            c2v_edge_index = torch.cat([data[('cons', 'to', 'vals')].edge_index,
-                                        torch.from_numpy(new_edge_index).long()], dim=1)
-            c2v_edge_attr = torch.cat([data[('cons', 'to', 'vals')].edge_attr,
-                                       torch.from_numpy(new_edge_attr[:, None]).float()], dim=0)
+            new_b = mat @ data.b.numpy()
+            bias = np.random.randn(new_b.shape[0])
+            bias[:m] = 0
+            new_b += bias
+            new_b = torch.from_numpy(new_b).float()
 
             new_data = data.__class__(
                 cons={
@@ -110,8 +106,8 @@ class AddRedundantConstraint:
                     'num_nodes': n,
                     'x': data['vals'].x,
                 },
-                cons__to__vals={'edge_index': c2v_edge_index,
-                                'edge_attr': c2v_edge_attr},
+                cons__to__vals={'edge_index': edge_index,
+                                'edge_attr': edge_attr},
                 q=data.q,
                 b=new_b,
                 obj_solution=data.obj_solution,
