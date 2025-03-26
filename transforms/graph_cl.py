2c6cc94b3878f12f61fb54e13eaa024d3beba0ff
Chendi Qian <1049653012@qq.com>
2025-03-26 16:25:53 +0100

    combo class

--- transforms/graph_cl.py
+++ transforms/graph_cl.py
@@ -1,3 +1,5 @@
+from typing import Dict, Tuple
+
 import torch
 from torch_geometric.data import HeteroData
 from torch_geometric.utils import bipartite_subgraph, add_random_edge
@@ -118,3 +120,35 @@ class GraphCLMaskNode:
             obj_solution=data.obj_solution,  # this is actually not correct
         )
         return new_data
+
+
+class ComboGraphCL:
+    def __init__(self, tf_dict: Dict):
+        strengths = tf_dict.values()
+        assert max(strengths) > 0, "At least 1 transformation!"
+
+        if 'GraphCLPerturbEdge' in tf_dict and tf_dict['GraphCLPerturbEdge'] > 0:
+            self.tf_edge = GraphCLPerturbEdge(tf_dict['GraphCLPerturbEdge'])
+        else:
+            self.tf_edge = lambda x: x
+
+        if 'GraphCLDropNode' in tf_dict and tf_dict['GraphCLDropNode'] > 0:
+            self.tf_node = GraphCLDropNode(tf_dict['GraphCLDropNode'])
+        else:
+            self.tf_node = lambda x: x
+
+        if 'GraphCLMaskNode' in tf_dict and tf_dict['GraphCLMaskNode'] > 0:
+            self.tf_mask = GraphCLMaskNode(tf_dict['GraphCLMaskNode'])
+        else:
+            self.tf_mask = lambda x: x
+
+    def __call__(self, data: HeteroData) -> Tuple[HeteroData, HeteroData]:
+        d1 = d2 = data
+
+        d1 = self.tf_edge(d1)
+        d2 = self.tf_edge(d2)
+        d1 = self.tf_node(d1)
+        d2 = self.tf_node(d2)
+        d1 = self.tf_mask(d1)
+        d2 = self.tf_mask(d2)
+        return d1, d2
