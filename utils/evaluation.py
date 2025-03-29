d84a297f57d5a369559e7638dc159069bd150ebe
Chendi Qian <1049653012@qq.com>
2025-03-29 16:39:24 +0100

    not sorted

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -14,8 +14,7 @@ def recover_lp_from_data(data, dtype=np.float32):
     A = SparseTensor(row=data['cons', 'to', 'vals'].edge_index[0],
                      col=data['cons', 'to', 'vals'].edge_index[1],
                      value=data['cons', 'to', 'vals'].edge_attr.squeeze(),
-                     sparse_sizes=(data['cons'].num_nodes, data['vals'].num_nodes),
-                     is_sorted=True, trust_data=True).to_dense().numpy().astype(dtype)
+                     sparse_sizes=(data['cons'].num_nodes, data['vals'].num_nodes)).to_dense().numpy().astype(dtype)
     # todo: might vary
     lb = np.zeros(A.shape[1]).astype(dtype)
     ub = None
