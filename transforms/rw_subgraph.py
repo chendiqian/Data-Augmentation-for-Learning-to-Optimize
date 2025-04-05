5756cfeeb1161639f3a81ebc641bdc379e2824bf
Chendi Qian <1049653012@qq.com>
2025-04-05 22:30:40 +0200

    implement qp graphcl tf

--- transforms/rw_subgraph.py
+++ transforms/rw_subgraph.py
@@ -1,6 +1,9 @@
+import torch
 from torch_cluster import random_walk
 from torch_geometric.data import HeteroData
-from torch_geometric.utils import bipartite_subgraph, to_undirected, degree
+from torch_geometric.utils import subgraph, bipartite_subgraph, to_undirected, degree
+
+from utils.evaluation import is_qp
 
 
 class RWSubgraph:
@@ -11,11 +14,14 @@ class RWSubgraph:
         self.walk_length = walk_length
 
     def __call__(self, data: HeteroData) -> HeteroData:
+        _is_qp = is_qp(data)
         m, n = data['cons'].num_nodes, data['vals'].num_nodes
 
         homo_edge_index = data['cons', 'to', 'vals'].edge_index.clone()  # CANNOT IN-SPACE!!!!
         homo_edge_index[1] += m
         homo_edge_index = to_undirected(homo_edge_index, num_nodes=m + n)
+        if _is_qp:
+            homo_edge_index = torch.hstack([homo_edge_index, data['vals', 'to', 'vals'].edge_index + m])
         deg = degree(homo_edge_index[0], m + n)
 
         # GCC starts with the one with max degree
@@ -54,4 +60,13 @@ class RWSubgraph:
             b=data.b[cons],
             obj_solution=data.obj_solution,  # this is actually not correct
         )
+
+        if _is_qp:
+            v2v_edge_index, v2v_edge_attr = subgraph(subset=vals,
+                                                     edge_index=data[('vals', 'to', 'vals')].edge_index,
+                                                     edge_attr=data[('vals', 'to', 'vals')].edge_attr,
+                                                     relabel_nodes=True,
+                                                     num_nodes=n)
+            new_data[('vals', 'to', 'vals')].edge_index = v2v_edge_index
+            new_data[('vals', 'to', 'vals')].edge_attr = v2v_edge_attr
         return new_data
