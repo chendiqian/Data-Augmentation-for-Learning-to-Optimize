c444a128bc32b5614f1483de681c5cb58e8aeafa
Chendi Qian <1049653012@qq.com>
2025-04-05 20:17:07 +0200

    clean code

--- data/collate_func.py
+++ data/collate_func.py
@@ -1,5 +1,4 @@
 from typing import List, Tuple
-import itertools
 
 import torch
 from torch_geometric.data import Data, Batch
@@ -11,10 +10,11 @@ def collate_fn_lp_base(graphs: List[Data]):
     flip_tensor = torch.tensor([1, 0])
     for k, v in graphs[0].edge_index_dict.items():
         src, rel, dst = k
-        new_batch[(dst, rel, src)].edge_index = new_batch[(src, rel, dst)].edge_index[flip_tensor]
-        new_batch[(dst, rel, src)].edge_attr = new_batch[(src, rel, dst)].edge_attr
+        if src != dst:
+            new_batch[(dst, rel, src)].edge_index = new_batch[(src, rel, dst)].edge_index[flip_tensor]
+            new_batch[(dst, rel, src)].edge_attr = new_batch[(src, rel, dst)].edge_attr
 
-    if not hasattr(new_batch[('vals', 'to', 'cons')], 'norm'):
+    if not hasattr(new_batch[('cons', 'to', 'vals')], 'norm'):
         norm_dict = {}
         for k, v in new_batch.edge_index_dict.items():
             norm_dict[k] = None
@@ -28,10 +28,3 @@ def collate_pos_pair(graphs: List[Tuple[Data, Data]]):
     batch1 = collate_fn_lp_base(graphs1)
     batch2 = collate_fn_lp_base(graphs2)
     return batch1, batch2
-
-
-def collate_pos_neg_pair(lists_graphs: List[List[Data]]):
-    data = [ls.pop(0) for ls in lists_graphs]
-    pos = [ls.pop(0) for ls in lists_graphs]
-    negs = list(itertools.chain(*lists_graphs))
-    return collate_fn_lp_base(data), collate_fn_lp_base(pos), collate_fn_lp_base(negs)
