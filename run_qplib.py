d1fade1338198496bbfb7d3a97904686498e8a58
Chendi Qian <chendiqian0111@gmail.com>
2025-05-06 23:33:31 +0800

    qplib

---
+++ run_qplib.py
@@ -0,0 +1,72 @@
+import hydra
+import numpy as np
+import torch
+import wandb
+from omegaconf import DictConfig
+from torch import optim
+from torch.utils.data import DataLoader
+
+from data.collate_func import collate_fn_lp_base
+from data.dataset import LPDataset
+from utils.experiment import save_run_config, setup_wandb
+from utils.evaluation import is_qp
+from utils.models import count_parameters
+from models.hetero_gnn import GNN
+from trainers.supervised_trainer import PlainGNNTrainer
+from trainers.training_loops import supervised_train_loops
+from transforms.gcn_norm import GCNNorm
+
+
+@hydra.main(version_base=None, config_path='./config', config_name="run_qplib")
+def main(args: DictConfig):
+    log_folder_name = save_run_config(args)
+    setup_wandb(args)
+
+    transform = GCNNorm() if 'gcn' in args.backbone.conv else None
+
+    train_subset = LPDataset(args.exp.datapath, 'train', transform=transform)
+
+    train_loader = DataLoader(train_subset,
+                              batch_size=args.finetune.batchsize,
+                              shuffle=True,
+                              collate_fn=collate_fn_lp_base,
+                              pin_memory=True)
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    train_losses = []
+
+    for run in range(args.exp.runs):
+        model = GNN(is_qp=is_qp(train_subset[0]),
+                    conv=args.backbone.conv,
+                    hid_dim=args.backbone.hidden,
+                    num_encode_layers=args.backbone.num_encode_layers,
+                    num_conv_layers=args.backbone.num_conv_layers,
+                    num_pred_layers=args.finetune.num_pred_layers,
+                    num_mlp_layers=args.backbone.num_mlp_layers,
+                    backbone_pred_layers=args.backbone.num_pred_layers,
+                    norm=args.backbone.norm).to(device)
+
+        optimizer = optim.Adam(model.parameters(), lr=args.finetune.lr, weight_decay=args.finetune.weight_decay)
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer,
+                                                         mode='min',
+                                                         factor=0.5,
+                                                         patience=100,
+                                                         min_lr=1.e-5)
+
+        trainer = PlainGNNTrainer()
+
+        best_model = supervised_train_loops(args.finetune.epoch, args.finetune.patience, args.exp.ckpt,
+                                            run, 0, log_folder_name,
+                                            trainer, train_loader, model, optimizer, scheduler)
+
+        train_losses.append(trainer.best_objgap)  # the training loss
+
+    wandb.log({
+        'num_params': count_parameters(model),
+        'train_loss_mean': np.mean(train_losses),
+        'train_loss_std': np.std(train_losses)
+    })
+
+
+if __name__ == '__main__':
+    main()
